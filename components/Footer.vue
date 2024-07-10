fb5ff61addd4849a4159803f874eb61b72caac12
ack20a <75193792+ack20a@users.noreply.github.com>
2024-07-10 20:39:23 +0800

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import {version} from '../package.json'
 </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4KTYN4E5D4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-4KTYN4E5D4');
+</script>
 <template>
 </template>
