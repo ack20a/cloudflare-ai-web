f686240c0eb27adc792df2518cfa4522dd4c77a7
ack20a <75193792+ack20a@users.noreply.github.com>
2024-07-10 20:53:04 +0800

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,14 +3,20 @@
 
 <script setup lang="ts">
 import { version } from '../package.json'
+import { onMounted } from 'vue';
 
-// Google tag (gtag.js) - moved inside the <script setup> tag
-if (process.client) { // Only execute on the client-side
+onMounted(() => {
+  // Load Google tag (gtag.js) asynchronously
+  const script = document.createElement('script');
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-4KTYN4E5D4';
+  script.async = true;
+  document.head.appendChild(script);
+
+  // Initialize gtag
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'G-4KTYN4E5D4');
-}
-</script>
+});
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-4KTYN4E5D4"></script> 
+</script>
