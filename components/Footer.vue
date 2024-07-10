21be0eeb803936a4c057127296742fc3cc31098e
ack20a <75193792+ack20a@users.noreply.github.com>
2024-07-10 20:48:51 +0800

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,15 +2,15 @@
 </template>
 
 <script setup lang="ts">
-import {version} from '../package.json'
-</script>
+import { version } from '../package.json'
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-4KTYN4E5D4"></script>
-<script>
+// Google tag (gtag.js) - moved inside the <script setup> tag
+if (process.client) { // Only execute on the client-side
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'G-4KTYN4E5D4');
+}
 </script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4KTYN4E5D4"></script> 
