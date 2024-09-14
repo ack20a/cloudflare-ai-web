8af14eae39ad2e5aa3abe3c9cdc20aff2d04e9e1
ack20a <75193792+ack20a@users.noreply.github.com>
2024-09-14 17:36:08 +0800

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -69,7 +69,6 @@ export type Settings = typeof initialSettings
 
 export const uniModals: Model[] = [
   { id: 'gemini-1.5-pro-exp-0827', name: 'Gemini 1.5 Pro Experiment 0827', provider: 'google', type: 'universal' },
-  { id: 'gemini-1.5-pro-search', name: 'Gemini 1.5 Pro Search', provider: 'google', type: 'universal' },
   { id: 'gemini-1.5-flash-exp-0827', name: 'Gemini 1.5 flash Experiment 0827', provider: 'google', type: 'universal' }
 ];
 
