f0bca89a57dfdaa986c334535955b55084f07300
ack20a <75193792+ack20a@users.noreply.github.com>
2024-11-16 19:28:43 +0800

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -68,18 +68,18 @@ export const initialSettings = {
 export type Settings = typeof initialSettings
 
 export const uniModals: Model[] = [
-  { id: 'gemini-1.5-pro-002', name: 'Gemini 1.5 Pro 002', provider: 'google', type: 'universal' },
+  { id: 'gemini-exp-1114', name: 'Gemini Experimental 1114', provider: 'google', type: 'universal' },
+  { id: 'llama-3.1-sonar-large-128k-online', name: 'Search', provider: 'openai', type: 'universal' },
   { id: 'gpt-4o', name: 'GPT-4o', provider: 'openai', type: 'universal' },
+  { id: 'claude-3-5-sonnet-20241022', name: 'Claude-3-5-sonnet', provider: 'openai', type: 'universal' }
+  { id: 'gemini-1.5-pro-002', name: 'Gemini 1.5 Pro 002', provider: 'google', type: 'universal' },
   { id: 'mistral-large-latest', name: 'Mistral Large', provider: 'openai', type: 'universal' },
   { id: 'llama-3.2-90b-vision-preview', name: 'Llama 3.2 90B (Preview)', provider: 'openai', type: 'universal' },
-  { id: 'ack-4o', name: 'ACK-4o', provider: 'openai', type: 'universal' },
-  { id: 'ack-search', name: 'ACK-Search', provider: 'openai', type: 'universal' },
-  { id: 'claude-3-5-sonnet-20240620', name: 'claude-3-5-sonnet', provider: 'openai', type: 'universal' }
 ];
 
 export const textGenModels: Model[] = [
   {id: 'gpt-4o-mini', name: 'GPT-4o mini', provider: 'openai', type: 'chat'},
-  {id: 'claude-3-haiku', name: 'Claude 3 Haiku', provider: 'openai', type: 'chat'},
+  {id: 'claude-3-5-haiku-20241022', name: 'Claude 3 Haiku', provider: 'openai', type: 'chat'},
   {id: '@cf/deepseek-ai/deepseek-math-7b-instruct', name: 'DeepSeekMath 7b 极速版', provider: 'workers-ai', type: 'chat'}
 ]
 
